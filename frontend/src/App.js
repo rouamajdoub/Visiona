2e4ae4938da8164b387f309782cf65b4015ba9f8
Roua Mejdoub <majdoubroua123@gmail.com>
2025-03-27 03:34:18 +0100

    client home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import Signup from "./pages/auth/Signup.jsx";
 import Dashboard from "./pages/Dashboard/Admin/AdminDashboard.jsx";
 import ArchitectDashboard from "./pages/Dashboard/Architect/Main.jsx";
 import Calender from "./pages/Dashboard/Architect/pages/Calendar/Calendar.jsx";
-import HomePage from "./pages/client_home_page/Home.jsx";
+import  {Home} from "./pages/client_home_page/Home.jsx";
 
 function App() {
   return (
@@ -15,10 +15,10 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/architect" element={<ArchitectDashboard />} />
         <Route path="/admin_dashboard" element={<Dashboard />} />
-        <Route path="/home" element={<HomePage />} />
+        <Route path="/home" element={<Home/>} />
         <Route path="/calender" element={<Calender />} />
 
-        <Route path="*" element={<HomePage />} />
+        <Route path="*" element={<Home />} />
       </Routes>
     </>
   );
